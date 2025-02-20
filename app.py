0a1356ef684febc03d2b99ba7f4a104988cd01b4
Abishek <abishek.123.prasad@gmail.com>
2025-02-20 18:42:00 +0530

    code files are ready

---
+++ app.py
@@ -0,0 +1,5 @@
+import streamlit
+import os
+from dotenv import load_dotenv
+from agents import ReasearchAgent
+from data_loader import DataLoader
---
+++ data_loader.py
@@ -0,0 +1,7 @@
+import requests
+import xml.etree.cElementTree as ET
+from scholarly import scholarly
+
+
+class DataLoader():
+    pass
